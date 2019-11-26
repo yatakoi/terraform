589f5f9853fb12627832103157ea9d816b7cd0f3
yatakoi <allkall@yandex.ru>
2019-11-26 14:21:35 +1000

    Add new aws_instanse and add security_group

--- main.tf
+++ main.tf
@@ -5,7 +5,35 @@ provider "aws" {
 resource "aws_instance" "first-server" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
+
 tags            =  {
   Name          = "terraform"
+ }
 }
+
+resource "aws_instance" "web-server" {
+  ami		= "ami-40d28157"
+  instance_type = "t2.micro"
+  vps_security_group_ids = ["${aws_security_group.instance.id}"]
+
+user_data	= <<-EOF
+		  #!/bin/bash
+		  echo "Hello, World" > index.html
+		  nohup busybox httpd -f -p 8080 &
+		  EOF
+
+tags            =  {
+  Name          = "web-server"
+ }
+}
+
+resource "aws_security_group" "instance" {
+  name		= "terraform-instance"
+
+  ingress {
+    from_port	= 8080
+    to_port	= 8080
+    protocol	= "tcp"
+    cidr_blocks	= ["0.0.0.0/0"]
+  }
 }
